cfd67aade7e09f390e738281714522dd16365342
Christoph Fausak <christoph.fausak@glueckkanja.com>
2016-07-19 11:16:05 +0200

    shutter, switch an light accessories implemented

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 Gira/Jung eNet plugin for homebridge: https://github.com/nfarina/homebridge
 
-# +++ UNDER DEVELOPMENT +++ UNDER DEVELOPMENT +++ UNDER DEVELOPMENT +++
-## No usability yet - checkin in a few weeks for usable code
-# +++ UNDER DEVELOPMENT +++ UNDER DEVELOPMENT +++ UNDER DEVELOPMENT +++
+This plugin can communicate with Jung/Gira Mobile Gateways and their provisioned devices.
+Currently, the whole setup needs to be done with the respective Jung/Gira eNet app.
+Then you need to get the channels for your devices. You might guess them - first used channel is 16.
+Or you use the sampe-gateway.js from the homebridge-enet package to read the config of your gateway.
+
 
 # Installation
 
@@ -20,17 +22,53 @@ Configuration sample:
 
      "platforms": [
              {
-               "platform": "eNetPlatform",
-               "name": "eNet",
-               "gateways" : ["1.1.1.1", "myGateway.local"],
+               "platform": "eNet",
+               "name": "eNet platform",
+               "gateways" : ["1.1.1.1", "myGateway.local"]
              }   
          ]
+     "platforms": [
+         {
+             "platform": "eNet Platform",
+             "name": "eNet",
+             "autodiscover": true,
+             "gateways": [{
+                 "name": "Mobile Gate",
+                 "mac": null,
+                 "host": null,
+                 "accessories": [
+                     {
+                         "channel": 16,
+                         "name": "Kitchen",
+                         "type": "Shutter",
+                     }
+                 ]
+             }]
+         }
+     ]
  }
 
  ```
 
 
-`gateways` is **optional**, defines an array of hostnames or ip addresses of your eNet gateways. Default is to autodiscover gateways using broadcasts.
+`autodiscover` is **optional**. If set to false, no broadcast discovery takes place, all gateways need to have the "host" property set. Default is to autodiscover gateways using broadcasts.
+
+`gateways` is a list of gateways.
+
+For each gateway, you need to give an identification, which will be used to find the gateway on the network. You have three possibilities:
+1. `host` - if you provide a hostname or ip address that is the identification for the gateway.
+2. `mac` - you can specify the mac-address of the gateway, e.g. by running sampe-discovery.js from the homebridge-enet package.
+3. `name` - identify gateway by its name. You can set this name with the Jung/Gira eNet app. Factory default is "Mobile Gate"
+
+`accessories` is a list of defined accessories on the gateway. Every accessory has the following properies:
+   `channel` - The eNet channel assigned to the accessory.
+   `name` - The HomeKit name of the accessory.
+   `type` - Type of accessory. Currently supported:
+      `Shutter` - Window covering accessory. You can set the target position.
+      `Switch` - An on/off switch.
+      `Light` - Same as `Switch`, but with a Lightbulb icon on Homekit.
+
+
 
 
 # License
--- index.js
+++ index.js
@@ -1,95 +1,264 @@
 "use strict";
 
-var Service, Characteristic;
+const eNet = require('node-enet-api');
+
+var Service, Characteristic, UUIDGen;
 
 module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
+    UUIDGen = homebridge.hap.uuid;
 
-    homebridge.registerPlatform("homebridge-eNet", "eNetPlatform", eNetPlatform);
+    homebridge.registerPlatform("homebridge-eNet", "eNet", eNetPlatform); //, true);
 }
 
 
-function eNetPlatform(log, config){
+function eNetPlatform(log, config, api) {
     this.log = log;
     this.config = config;
     this.gateways = Array.isArray(config.gateways) ? config.gateways : [];
+    this.accessories = [];
+    this.gateways = [];
+    this.loadState = 2; // didFinishLaunching & discover
+
+    var discover = new eNet.discover();
+
+    discover.on('discover', this.newGateway});
+
+    if (api) {
+        this.api = api;
+
+        this.api.on('didFinishLaunching', function() {
+            if (--this.loadState === 0) this.setupDevices();
+        }.bind(this));
+    }
+    else --this.loadState;
+
+    discover.discover(function(err) {
+        if (err) log.console.warn('Discovery error: ' + err);
+        if (--this.loadState === 0) this.setupDevices();
+    }.bind(this));
 };
 
-eNetPlatform.prototype.accessories = function(callback) {
-    callback([new eNetShutterAccessory(this.log, "just a test", 16, "TestAccessory")]);
+eNetPlatform.prototype.newGateway = function (gw) {
+    ++this.loadState;
+    var g = new eNet.gateway(gw);
+
+    g.getChannelInfo(function(err, res) {
+        if (!err)
+        {
+            if (res && Array.isArray(res.DEVICES)) {
+                g.devices = res.DEVICES;
+                this.gateways.push(g);
+            }
+            else err = JSON.stringify(res);
+        }
+
+        if (err) this.log.warn("Failed to get gateway channels, ignoring gateway. Error: " + err);
+        if (--this.loadState === 0) this.setupDevices();
+    }.bind(this));
+};
+
+eNetPlatform.prototype.setupDevices = function() {
+    // connect cached accessories to gateways
+    for (var i = 0; i < this.accessories.length; ++i) {
+        var acessory = this.accessories[i];
+        if (!accessories.gateway) {
+            if (accessory.gateway = this.findGateway(accessory.context.gateID)) {
+                accessory.reachable = true;
+            }
+        }
+    }
+
+    var addedDevices = false;
+
+    if (Array.isArray(this.config.gateways)) {
+        for (var i = 0; i < this.config.gateways.length; ++i) {
+            var gw = this.config.gateways[i];
+            if (Array.isArray(g.accessories)) {
+                var g;
+                if (gw.host) g = this.findGateway(gw.host);
+                if (!g && gw.mac) g = this.findGateway(gw.mac);
+                if (!g && gw.name) g = this.findGateway(gw.name);
+
+                if (!g && gw.host) {
+                    g = new eNet.gateway(gw);
+                    g.getChannelInfo(function(err, res) {
+                        if (!err)
+                        {
+                            if (res && Array.isArray(res.DEVICES)) {
+                                g.devices = res.DEVICES;
+                                this.gateways.push(g);
+                            }
+                            else err = JSON.stringify(res);
+                        }
+
+                        if (err) this.log.warn("Failed to get gateway channels, ignoring gateway. Error: " + err);
+                        if (--this.loadState === 0) this.setupDevices();
+                    }.bind(this));
+                }
+
+                if (g) {
+                    for (var j = 0; j < gw.accessories; ++j) {
+                        var acc = gw.accessories[j];
+
+                        a = findAccessory(g.id, acc.channel);
+                        if (a && a.context.type != acc.type) {
+                            // kick this accessory out, create new one
+                            a.reachable = false;
+                            this.createAccessory(g.id, acc);
+                        }
+                    }
+                }
+                else this.log.warn("Cannot find gateway: " + JSON.stringify(g));
+            }
+        }
+    }
+
+    var keep = [], del[];
+    for (var i = 0; i < this.accessories.length; ++i) {
+        if (this.accessories[i].reachable) keep.push(this.accessories[i]);
+        else del.push(this.accessories[i]);
+    }
+
+    this.accessories = keep;
+    if (del.length) this.api.unregisterPlatformAccessories("homebridge-eNet", "eNet", del);
 }
 
-function eNetShutterAccessory(log, gateway, channel, name) {
-	this.log = log;
-    this.gateway = gateway;
-
-    this.channel = channel;
-    this.name = name;
-
-    this.position = 0;
-
-/*    Service.WindowCovering
-
-      // Required Characteristics
-      this.addCharacteristic(Characteristic.CurrentPosition);
-      this.addCharacteristic(Characteristic.TargetPosition);
-      this.addCharacteristic(Characteristic.PositionState);
-
-      // Optional Characteristics
-      this.addOptionalCharacteristic(Characteristic.HoldPosition);
-      this.addOptionalCharacteristic(Characteristic.TargetHorizontalTiltAngle);
-      this.addOptionalCharacteristic(Characteristic.TargetVerticalTiltAngle);
-      this.addOptionalCharacteristic(Characteristic.CurrentHorizontalTiltAngle);
-      this.addOptionalCharacteristic(Characteristic.CurrentVerticalTiltAngle);
-      this.addOptionalCharacteristic(Characteristic.ObstructionDetected);
-      this.addOptionalCharacteristic(Characteristic.Name);
-*/
-
-    this.service = new Service.WindowCovering(this.name);
-
-    this.service
-      .getCharacteristic(Characteristic.CurrentPosition)
-      .on('get', this.getCurrentPosition.bind(this));
-
-    this.service
-      .getCharacteristic(Characteristic.TargetPosition)
-      .on('get', this.getTargetPosition.bind(this))
-      .on('set', this.setTargetPosition.bind(this));
-
-    this.service
-      .addCharacteristic(Characteristic.PositionState)
-      .on('get', this.getPositionState.bind(this));
+eNetPlatform.prototype.findGateway = function(id) {
+    for (var i = 0; i < this.gateways.length; ++i) {
+        var gw = this.gateways[i];
+        if ((gw.mac === id) || (gw.name === id) || (gw.host === id)) return gw;
+    }
 }
 
-eNetShutterAccessory.prototype.getServices = function() {
-    return [this.service];
+eNetPlatform.prototype.findAccessory = function(gateID, channel) {
+    for (var i = 0; i < this.accessories.length; ++i) {
+        var a = this.accessories[i];
+        if ((a.context.gateID === gateID) && (a.context === channel)) return a;
+    }
 }
 
-eNetShutterAccessory.prototype.getCurrentPosition = function(callback) {
-  if (false) {
-    this.log.warn("eNet device not yet ready.");
-    callback(new Error("eNet device not yet ready."));
-    return;
-  }
+SamplePlatform.prototype.configureAccessory = function(accessory) {
+    if (this.setupAccessory(accessory)) {
+        this.accessories.push(accessory);
+    }
+}
 
+eNetPlatform.prototype.createAccessory = function(gateID, conf) {
+    this.log.info("createShutterAccessory");
+    var uuid;
+
+    if (!conf.name || (typeof conf.channel !== 'number')) {
+        thhis.log.warn("Cannot add accessory, invalid config: " JSON.stringify(conf));
+        return;
+    }
+
+    uuid = UUIDGen.generate(conf.name);
+
+    var accessory = new Accessory(conf.name, uuid);
+
+    if (conf.type === "Shutter") {
+        accessory.addService(Service.WindowCovering, "Jalousie")
+    }
+    else if (conf.type === "Light") {
+        accessory.addService(Service.Lightbulb, "Light")
+    }
+    else if (conf.type === "Switch") {
+        accessory.addService(Service.Switch, "Power")
+    }
+    else {
+        thhis.log.warn("Cannot add accessory, invalid config: " JSON.stringify(conf));
+        return;
+    }
+
+    accessory.context.gateID = gateID;
+    accessory.context.type = conf.type;
+    accessory.context.channel = conf.channel;
+
+    if (this.setupAccessory(accessory)) {
+        this.accessories.push(accessory);
+        this.api.registerPlatformAccessories("homebridge-samplePlatform", "SamplePlatform", [accessory]);
+    }
+}
+
+function eNetPlatform.prototype.setupAccessory(accessory) {
+    var service;
+
+    accessory.log = this.log;
+
+    if (service = accessory.getService(Service.Lightbulb)) {
+        service
+          .getCharacteristic(Characteristic.On)
+//          .on('get', getCurrentPosition.bind(accessory))
+          .on('set', setOn.bind(accessory));
+    }
+    else if (service = accessory.getService(Service.Switch) {
+        service
+          .getCharacteristic(Characteristic.On)
+//          .on('get', getCurrentPosition.bind(accessory))
+          .on('set', setOn.bind(accessory));
+    }
+    else if (service = accessory.getService(Service.WindowCovering)) {
+        this.position = 0;
+        this.targetPosition = 0;
+        this.positionState = Characteristic.PositionState.STOPPED;
+
+        service
+          .getCharacteristic(Characteristic.CurrentPosition)
+          .on('get', getCurrentPosition.bind(accessory))
+          .on('set', setCurrentPosition.bind(accessory));
+
+        service
+          .getCharacteristic(Characteristic.TargetPosition)
+          .on('get', getTargetPosition.bind(accessory))
+          .on('set', setTargetPosition.bind(accessory));
+
+        service
+          .getCharacteristic(Characteristic.PositionState)
+          .on('get', getPositionState.bind(accessory))
+          .on('set', setPositionState.bind(accessory)),
+          .value = this.positionState;
+
+    }
+    else return false;
+
+    accessory.on('identify', function(paired, callback) {
+        this.log.info("Identify: " + JSON.stringify(this.context));
+        callback();
+    }.bind(accessory));
+
+
+    if (accessory.gateway = this.findGateway(accessory.context.gateID)) {
+        accessory.reachable = true;
+    }
+    else accessory.reachable = false;
+
+    return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Accessory notifications
+//
+
+function getCurrentPosition(callback) {
   callback(null, this.position);
 }
 
-eNetShutterAccessory.prototype.getTargetPosition = function(callback) {
-  if (false) {
-    this.log.warn("eNet device not yet ready.");
-    callback(new Error("eNet device not yet ready."));
-    return;
-  }
+function setCurrentPosition(position, callback) {
+  this.position = position;
+  callback(null);
+}
 
+function getTargetPosition(callback) {
   callback(null, this.position);
 }
 
-eNetShutterAccessory.prototype.setTargetPosition = function(position, callback) {
-  if (false) {
-    this.log.warn("eNet device not yet ready.");
-    callback(new Error("eNet device not yet ready."));
+function setTargetPosition(position, callback) {
+  if (!this.gateway) {
+    this.log.warn("eNet device not ready.");
+    callback(new Error("eNet device not ready."));
     return;
   }
 
@@ -98,17 +267,27 @@ eNetShutterAccessory.prototype.setTargetPosition = function(position, callback) 
   callback(null);
 }
 
-eNetShutterAccessory.prototype.getPositionState = function(callback) {
-  if (false) {
-    this.log.warn("eNet device not yet ready.");
-    callback(new Error("eNet device not yet ready."));
+function getPositionState(callback) {
+  callback(null, this.positionState);
+}
+
+// Characteristic.PositionState.DECREASING = 0;
+// Characteristic.PositionState.INCREASING = 1;
+// Characteristic.PositionState.STOPPED = 2;
+function setPositionState(position, callback) {
+  this.positionState = position;
+  callback(null);
+}
+
+function setOn(position, callback) {
+  if (!this.gateway) {
+    this.log.warn("eNet device not ready.");
+    callback(new Error("eNet device not ready."));
     return;
   }
 
-  // The value property of PositionState must be one of the following:
-  // Characteristic.PositionState.DECREASING = 0;
-  // Characteristic.PositionState.INCREASING = 1;
-  // Characteristic.PositionState.STOPPED = 2;
+  this.gateway.setValue(this.context.channel, position, false, callback);
 
-  callback(null, Characteristic.PositionState.STOPPED);
+// todo
+  callback(null);
 }
